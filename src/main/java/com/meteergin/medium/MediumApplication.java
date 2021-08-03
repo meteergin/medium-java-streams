f82ed97a8f69bad32df62630b530b09b316ab7b9
Mete Ergin <mete.ergin@outlook.com>
2021-08-03 20:42:55 +0300

    first commit

---
+++ src/main/java/com/meteergin/medium/MediumApplication.java
@@ -0,0 +1,13 @@
+package com.meteergin.medium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meteergin/medium/MediumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteergin.medium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
