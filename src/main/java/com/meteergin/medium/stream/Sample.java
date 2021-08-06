e446501b45c02af3f25e2d3a47761f6fcd24554d
Mete Ergin <mete.ergin@outlook.com>
2021-08-06 23:12:58 +0300

    Stream article sample

--- src/main/java/com/meteergin/medium/stream/Sample.java
+++ src/main/java/com/meteergin/medium/stream/Sample.java
@@ -1,42 +1,134 @@
 package com.meteergin.medium.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author Mete Ergin
  */
 public class Sample {
-    private List<User> userList = Arrays.asList(
-            new User(1, "James", "Robert", 37),
+
+    private final List<User> userList = Arrays.asList(
+            new User(1, "Michael", "Robert", 37),
             new User(2, "Mary", "Patricia", 11),
             new User(3, "John", "Michael", 7),
             new User(4, "Jennifer", "Linda", 77),
-            new User(5, "William", "Elizabeth", 23)
+            new User(5, "William", "Elizabeth", 23),
+            new User(6, "Sue", "Jackson", 11),
+            new User(7, "Michael", "Tommy", 37)
     );
 
     public static void main(String... args) {
         Sample sample = new Sample();
         sample.test1();
         sample.test2();
+        sample.test3();
+        sample.test4();
+        sample.test5();
+        sample.test6();
+        sample.test7();
+        sample.test8();
     }
 
     private void test1() {
         System.out.println("Test 1");
-        
-        userList.stream().forEach(System.out::println);
+
+        userList.stream().
+                forEach(System.out::println);
     }
 
     private void test2() {
         System.out.println("Test 2");
-        
-        userList.stream().map(u -> {
-            return new User(u.getId(), "X " + u.getFirstName(), "Y " + u.getLastName(), u.getAge() + 10);
-        }).collect(Collectors.toList())
-                .forEach(System.out::println);
 
-        userList.stream().forEach(System.out::println);
+        userList.stream()
+                .map(u -> {
+                    return new User(
+                            u.getId(),
+                            "X " + u.getFirstName(),
+                            "Y " + u.getLastName(),
+                            u.getAge() + 10);
+                })
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    private void test3() {
+        System.out.println("Test 3");
+
+        userList.stream()
+                .sorted(Comparator.comparing(User::getAge))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    private void test4() {
+        System.out.println("Test 4");
+
+        userList.stream()
+                .sorted(Comparator.comparing(User::getAge)
+                        .thenComparing(User::getFirstName)
+                        .thenComparing(User::getLastName))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    private void test5() {
+        System.out.println("Test 5");
+
+        double averageAge = userList.stream()
+                .mapToInt(User::getAge)
+                .summaryStatistics()
+                .getAverage();
+
+        System.out.print("averageAge: " + averageAge);
+
+        int maxFirstNameLenght = userList.stream()
+                .mapToInt(user -> {
+                    return user.getFirstName().length();
+                })
+                .summaryStatistics()
+                .getMax();
+
+        System.out.println(" maxFirstNameLenght: " + maxFirstNameLenght);
+    }
+
+    private void test6() {
+        System.out.println("Test 6");
+
+        boolean isAllAgesGreaterThan6 = userList.stream()
+                .allMatch(user -> user.getAge() > 6);
+
+        System.out.println("isAllAgesGreaterThan6: " + isAllAgesGreaterThan6);
+    }
+
+    private void test7() {
+        System.out.println("Test 7");
+        boolean isFirstCharS = userList.stream()
+                .anyMatch(user -> user.getFirstName().charAt(0) == 'S');
+
+        System.out.println("isFirstCharS: " + isFirstCharS);
+    }
+
+    private void test8() {
+        System.out.println("Test 8");
+
+        List<User> list = userList.stream()
+                .collect(Collectors.toList());
+
+        Set<User> set = userList.stream()
+                .collect(Collectors.toSet());
+
+        List<User> linkedList = userList.stream()
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        Map<Long, User> map = userList.stream()
+                .collect(Collectors.toMap(user -> user.getId(), user -> user));
     }
 
 }
